c233af9f009e1a27d8d1f40d1a6ad7d6e6ddf91d
Sivaranjani212005 <sivaranjanisshunmugaraj@gmail.com>
2025-06-02 11:58:06 +0530

    TimeIntervalSelector.js

--- src/Component/TimeIntervalSelector.js
+++ src/Component/TimeIntervalSelector.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 const TimeIntervalSelector = ({ value, onChange }) => {
   return (
     <div className="time-selector">
@@ -15,5 +14,4 @@ const TimeIntervalSelector = ({ value, onChange }) => {
     </div>
   );
 };
-
 export default TimeIntervalSelector;
